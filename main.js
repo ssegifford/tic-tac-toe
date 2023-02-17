99a2776112de1163377cc32ddfb2cf8763311586
ssegifford <eng.gifford@gmail.com>
2023-02-17 18:40:27 -0500

    make a mark

--- main.js
+++ main.js
@@ -1,1 +1,28 @@
 console.log('Your JS is linked up. Be the person you needed when you were little.');
+
+/*----- constants -----*/
+/*----- app's state (variables) -----*/
+let board;
+
+
+/*----- cached element references -----*/
+// The Array.from() function will make an array from all elements returned by querySelectorAll
+const squares = Array.from(document.querySelectorAll('#board div'));
+
+
+/*----- event listeners -----*/
+
+/*----- functions -----*/
+ //function to initiate game board
+const init = () => {
+    board = ['', '', '','', '', '','', '', ''];    
+    render();
+}   
+
+// rendering a change in the game when a mark is made
+const render = () =>{
+    board.forEach(function(mark, index){
+        //this sets the text content of the square of the same position to the mark on the board. 
+        squares[index].textContent = mark;
+        });
+}
